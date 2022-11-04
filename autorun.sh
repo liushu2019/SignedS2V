8461c8ed99a654adbb1a7911404d1c6ac72c5e27
liu <liu@torilab.net>
2022-11-04 17:11:32 +0900

    [NEW] SignedS2V

---
+++ autorun.sh
@@ -0,0 +1,2 @@
+python src/main.py --input graph/${1}_sign.edgelist --num-walks 20 --walk-length 80 --window-size 5 --dimensions 2 --until-layer 5 --workers 8 --OPT1 --OPT2 --OPT3 --scalefree
+#sh autorun.sh mirrored_karate
